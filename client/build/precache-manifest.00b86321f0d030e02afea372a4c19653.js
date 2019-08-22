5639573bfaa795037cf32d0733819e1e5ba83b08
zoesteel <zoeysteel@gmail.com>
2019-08-22 04:09:49 -0700

    build

---
+++ client/build/precache-manifest.00b86321f0d030e02afea372a4c19653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd18982e086c0f2e3c72e5158d58f53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26edacd520ebb0574db4",
+    "url": "/static/css/main.e7971381.chunk.css"
+  },
+  {
+    "revision": "d79aac1502473d9f72e0",
+    "url": "/static/js/2.3082319c.chunk.js"
+  },
+  {
+    "revision": "26edacd520ebb0574db4",
+    "url": "/static/js/main.a8a96e0c.chunk.js"
+  },
+  {
+    "revision": "3cdfac042bcaa62a8198",
+    "url": "/static/js/runtime~main.116d1035.js"
+  },
+  {
+    "revision": "7908f14719f208627a181b99f8becad2",
+    "url": "/static/media/sun.7908f147.png"
+  }
+]);
