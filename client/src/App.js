2cfa547d24f00e9100bfa3f68cbc11640ac7e651
zoesteel <zoeysteel@gmail.com>
2019-08-21 17:23:33 -0700

    Call API from express server

--- client/src/App.js
+++ client/src/App.js
@@ -10,58 +10,51 @@ class App extends Component {
         this.state = {
             city: '',
             temperature: '',
-            units: '',
-            symbol: '',
+            units: 'metric',
+            symbol: 'C',
             description: '',
             message: '',
-            error: undefined,
-            response: '',
-            post: '',
-            responseToPost: '',
+            error: undefined,            
         }        
-        this.handleSearch = this.handleSearch.bind(this);
     }
-
-    handleSearch(city, temperature, units, symbol, description, error) {
-        this.setState({
-            city: city,
-            temperature: temperature,
-            units: units,
-            symbol: symbol,
-            description: description,
-            error: error,
-            message: `The temperature in ${city} is ${temperature}°${symbol}`,
-        });
-    }
-
-
-    componentDidMount() {
-        this.callApi1()
-          .then(res => this.setState({ response: res.express }))
-          .catch(err => console.log(err));
-      }
       
-      callApi1 = async () => {
-        const response = await fetch('/api/hello');
-        const body = await response.json();
-        if (response.status !== 200) throw Error(body.message);
+    // callApi = async () => {
+    //     const response = await fetch('/weather');
+    //     const body = await response.json();
+    //     if (response.status !== 200) throw Error(body.message);
         
-        return body;
-      };
+    //     return body;
+    // };
       
-      handleSubmit1 = async e => {
+    handleSubmit = async e => {
         e.preventDefault();
-        const response = await fetch('/api/world', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ post: this.state.post }),
+        const response = await fetch('/weather', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ 
+                city: this.state.city,
+                units: this.state.units
+            }),
         });
-        const body = await response.text();
-        
-        this.setState({ responseToPost: body });
-      };
+       
+        const body = await response.json();
+        this.setState({ 
+            city: body.city,
+            temperature: body.temperature,
+            description: body.description,
+            message: `The temperature in ${body.city} is ${body.temperature}°${this.state.symbol}`,
+         });
+    };
+
+    // handles change in radio buttons for imperial or metric
+    handleUnitChange = (e) => {
+        this.setState({
+            units: e.target.value,
+            symbol: e.target.id,
+        })
+    }
 
     render () {
         return (
@@ -71,37 +64,63 @@ class App extends Component {
                     <h1 className="title">Check the weather</h1>
                     
                     {/* the input form  */}
-                    <CitySearch 
+                    {/* <CitySearch 
                         onSearchComplete={this.handleSearch}
                     />
-
+                     */}
+                    {/* code for api call within react */}
                     {/* if no error message exists then display the weather message */}
-                    { this.state.error === undefined &&
+                    {/* { this.state.error === undefined &&
                         <div>
                             <h2>{this.state.description}</h2>                    
                             <p>{this.state.message}</p>     
                         </div>
-                    }
+                    } */}
 
                     {/* if error exists then display it */}
                     { this.state.error !== undefined && 
                          <p>{this.state.error}</p>
                     }
 
-
-                    <p>{this.state.response}</p>
-                            <form onSubmit={this.handleSubmit1}>
-                            <p>
-                                <strong>Post to Server:</strong>
-                            </p>
-                            <input
-                                type="text"
-                                value={this.state.post}
-                                onChange={e => this.setState({ post: e.target.value })}
+                    <form onSubmit={this.handleSubmit} className="form">
+                        <input
+                            type="text"
+                            value={this.state.city}
+                            id="city"
+                            onChange={e => this.setState({ 
+                                city: e.target.value 
+                            })}
+                        />
+                        
+                        <div onChange={this.handleUnitChange}>
+                            <input 
+                                type="radio" 
+                                name="units" 
+                                value="metric" 
+                                id="C"
+                                defaultChecked
                             />
-                            <button type="submit">Submit</button>
-                            </form>
-                            <p> {this.state.response} {this.state.responseToPost}</p>
+                            <label 
+                                htmlFor="celcius">Celcius
+                            </label>
+                            <input 
+                                type="radio" 
+                                name="units" 
+                                value="imperial" 
+                                id="F" 
+                            />
+                            <label 
+                                htmlFor="fahrenheit">Fahrenheit
+                            </label>                            
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+
+                    <div>
+                        <h2>{this.state.description}</h2>                    
+                        <p>{this.state.message}</p>     
+                    </div>
+
                 </header>
             </div>
         );
--- server/index.js
+++ server/index.js
@@ -1,60 +1,64 @@
-// const express = require('express');
-// const app = express();
-// const bodyParser = require('body-parser');
-// const pino = require('express-pino-logger')();
-// const axios = require('axios');
-// const testAPIRouter = require('./routes/testAPI');
-// const cors = require('cors');
-
-// app.use(cors());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(pino);
-
-// // app.use("/testAPI", testAPIRouter);
-
-// app.get('/', (req, res) => {
-//     const city = req.query.city;
-//     const api_key = process.env.API_KEY;
-//     const url = `https://api.openweathermap.org/data/2.5/weather?q=london&APPID=d5566182730361aa1c00849d2f86bb95`;
-//     res.setHeader('Content-Type', 'application/json');
-//     axios(url)
-//         .then(res => res.json())
-//         .then(result => {
-//             console.log(result);
-//             res.send({ result });
-//         })
-//         .catch(err => {
-//             res.send(err);
-//         }); 
-//     });
-
-// // app.post('/', (req, res) => {
-// //     console.log(req.body);
-// //     res.send(
-// //         `${req.body}`,
-// //     );
-// //     });
-
-// app.listen(3001);
-
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const app = express();
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const axios = require('axios');
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(pino);
 const port = process.env.PORT || 3001;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/weather', (req, res) => { 
+    res.send({ express: `'Hello From Express'` });
+//     const city = req.query.city;
+//     const api_key = process.env.API_KEY;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+    // this was working aug 21 morning
+    // axios(url)
+    //     .then(res => res.json())
+    //     .then(result => {
+    //         console.log(result);
+    //         res.send({ express: result });
+    //     })
+    //     .catch(err => {
+    //         res.send(err);
+    //     }); 
+    // });
 });
 
-app.post('/api/world', (req, res) => {
+app.post('/weather', (req, res) => {
   console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
+  const city = req.body.city;
+  const units = req.body.units;
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&APPID=d5566182730361aa1c00849d2f86bb95`
+//   const api_key = process.env.API_KEY;
+//   res.send(
+//     `I received your POST request. This is what you sent me: ${req.body.post}`,
+//   );
+    
+  axios.get(url)
+    .then(response => {  
+        console.log(url)
+        const cityName = response.data.name; 
+        const temperature = response.data.main.temp;
+        const description = response.data.weather[0].main;
+        
+        res.send({
+            city: cityName,
+            temperature: temperature,
+            description: description
+        });
+
+    })   
+    .catch(err => {
+        res.send(err);
+    }); 
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+
+app.listen(port);
